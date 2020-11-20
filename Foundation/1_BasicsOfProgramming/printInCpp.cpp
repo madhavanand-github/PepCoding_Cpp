a245b2c56648bcdb73697aa8f9e536d7b90152bf
Madhav Anand <madhavanand402@gmail.com>
2020-11-20 12:48:26 +0530

    Print in Cpp Added

---
+++ Foundation/1_BasicsOfProgramming/printInCpp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+// "iostream" means Input Output Stream file includes code of "cout"
+using namespace std;
+// This is just to make sure scope is not limited
+
+/* At first Main Function is called &
+successful execution is indicated by 0 value returned */
+
+int main(){
+
+    // keyword - cout
+    // << - Insertion Operator
+    // Everything inside "Double Quotes" Prints as it is.
+    // Just like fullstop in english we have semicolon ';' in programming
+    cout<<"hello world\n";
+    // "/n" is just a fancy way of saying "Press Enter Key"
+    cout<<"bye"<<endl;
+    // "endl" is alternate of "\n", means "End Line"
+    cout<<"bye";
+
+    return 0;
+}
