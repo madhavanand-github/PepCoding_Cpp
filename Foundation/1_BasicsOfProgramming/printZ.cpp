e7558427003d3c66170a234c61d21f243a316af1
Madhav Anand <madhavanand402@gmail.com>
2020-11-20 13:07:21 +0530

    Print Z Added

---
+++ Foundation/1_BasicsOfProgramming/printZ.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Method #1
+    cout<<"Method #1"<<endl<<endl;
+    cout<<"*****"<<endl;
+    cout<<"   * "<<endl;
+    cout<<"  *  "<<endl;
+    cout<<" *   "<<endl;
+    cout<<"*****"<<endl;
+
+    cout<<endl;
+    
+    // Method #2
+    cout<<"Method #2"<<endl<<endl;
+    cout<<"*****\n";
+    cout<<"   * \n";
+    cout<<"  *  \n";
+    cout<<" *   \n";
+    cout<<"*****\n";
+
+    return 0;
+}
